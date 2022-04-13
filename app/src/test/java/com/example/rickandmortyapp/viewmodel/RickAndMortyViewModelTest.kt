243fea1d6d705484ef2eca61eec4239239759d73
afgar88 <90805867+afgar88@users.noreply.github.com>
2022-04-12 21:06:47 -0400

    Test view model success state

--- app/src/test/java/com/example/rickandmortyapp/viewmodel/RickAndMortyViewModelTest.kt
+++ app/src/test/java/com/example/rickandmortyapp/viewmodel/RickAndMortyViewModelTest.kt
@@ -1,18 +1,17 @@
 package com.example.rickandmortyapp.viewmodel
 
-import android.util.Log
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.rickandmortyapp.model.charactersmodel.Characters
 import com.example.rickandmortyapp.network.RickAndMortyRepository
 import com.example.rickandmortyapp.utils.RickAndMortyState
 import com.google.common.truth.Truth.assertThat
 import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.TestScope
-import kotlinx.coroutines.test.setMain
+import kotlinx.coroutines.test.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -21,10 +20,10 @@ import org.junit.Test
 @ExperimentalCoroutinesApi
 class RickAndMortyViewModelTest {
 
-    @get:Rule var rule = InstantTaskExecutorRule()
+    @get:Rule
+    var rule = InstantTaskExecutorRule()
 
-    private val testDispatcher = StandardTestDispatcher()
-    private val testCoroutinesScope = TestScope(testDispatcher)
+    private val testDispatcher = UnconfinedTestDispatcher()
 
     private val mockRepository = mockk<RickAndMortyRepository>(relaxed = true)
 
@@ -39,27 +38,139 @@ class RickAndMortyViewModelTest {
     @After
     fun shutdown() {
         clearAllMocks()
-
-        testCoroutinesScope.cancel()
+        Dispatchers.resetMain()
     }
 
     @Test
     fun `get all characters when trying to load from server returns loading state`() {
-        // AAA
-        // Assign
+
         val stateList = mutableListOf<RickAndMortyState>()
         target.allCharacters.observeForever {
             stateList.add(it)
         }
-        // Action
+
         target.getAllCharacters()
 
-        // Assert
+
         assertThat(stateList).isNotEmpty()
-        assertThat(stateList).hasSize(2)
         assertThat(stateList[0]).isInstanceOf(RickAndMortyState.LOADING::class.java)
         assertThat(stateList[1]).isInstanceOf(RickAndMortyState.LOADING::class.java)
 
 
     }
+
+    @Test
+    fun `get all characters when trying to load from server returns success state`() = runTest {
+
+        coEvery { mockRepository.getAllCharacters() } returns mockk {
+            every { isSuccessful } returns true
+            every { body() } returns mockk { mockk<Characters>() }
+        }
+
+        val stateList = mutableListOf<RickAndMortyState>()
+        target.allCharacters.observeForever {
+            stateList.add(it)
+        }
+
+        target.getAllCharacters()
+
+
+        assertThat(stateList).isNotEmpty()
+        assertThat(stateList).hasSize(3)
+        assertThat(stateList[0]).isInstanceOf(RickAndMortyState.LOADING::class.java)
+        assertThat(stateList[1]).isInstanceOf(RickAndMortyState.LOADING::class.java)
+        assertThat(stateList[2]).isInstanceOf(RickAndMortyState.SUCCESS::class.java)
+
+
+    }
+
+
+    @Test
+    fun `get all locations when trying to load from server returns loading state`() {
+
+        val stateList = mutableListOf<RickAndMortyState>()
+        target.allLocations.observeForever {
+            stateList.add(it)
+        }
+
+        target.getAllLocations()
+
+
+        assertThat(stateList).isNotEmpty()
+        assertThat(stateList[0]).isInstanceOf(RickAndMortyState.LOADING::class.java)
+        assertThat(stateList[1]).isInstanceOf(RickAndMortyState.LOADING::class.java)
+
+
+    }
+
+    @Test
+    fun `get all locations when trying to load from server returns success state`() = runTest {
+
+        coEvery { mockRepository.getAllLocations() } returns mockk {
+            every { isSuccessful } returns true
+            every { body() } returns mockk { mockk<Characters>() }
+        }
+
+        val stateList = mutableListOf<RickAndMortyState>()
+        target.allLocations.observeForever {
+            stateList.add(it)
+        }
+
+        target.getAllLocations()
+
+
+        assertThat(stateList).isNotEmpty()
+        assertThat(stateList).hasSize(3)
+        assertThat(stateList[0]).isInstanceOf(RickAndMortyState.LOADING::class.java)
+        assertThat(stateList[1]).isInstanceOf(RickAndMortyState.LOADING::class.java)
+        assertThat(stateList[2]).isInstanceOf(RickAndMortyState.SUCCESS::class.java)
+
+
+    }
+
+    @Test
+    fun `get all episodes when trying to load from server returns loading state`() = runTest {
+
+
+        val stateList = mutableListOf<RickAndMortyState>()
+        target.allepisodes.observeForever {
+            stateList.add(it)
+        }
+
+        target.getAllEpisodes()
+
+
+        assertThat(stateList).isNotEmpty()
+        assertThat(stateList[0]).isInstanceOf(RickAndMortyState.LOADING::class.java)
+        assertThat(stateList[1]).isInstanceOf(RickAndMortyState.LOADING::class.java)
+
+
+    }
+
+    @Test
+    fun `get all episodes when trying to load from server returns success state`() = runTest {
+
+        coEvery { mockRepository.getAllEpisodes() } returns mockk {
+            every { isSuccessful } returns true
+            every { body() } returns mockk { mockk<Characters>() }
+        }
+
+        val stateList = mutableListOf<RickAndMortyState>()
+        target.allepisodes.observeForever {
+            stateList.add(it)
+        }
+
+        target.getAllEpisodes()
+
+
+        assertThat(stateList).isNotEmpty()
+        assertThat(stateList).hasSize(3)
+        assertThat(stateList[0]).isInstanceOf(RickAndMortyState.LOADING::class.java)
+        assertThat(stateList[1]).isInstanceOf(RickAndMortyState.LOADING::class.java)
+        assertThat(stateList[2]).isInstanceOf(RickAndMortyState.SUCCESS::class.java)
+
+
+    }
+
+
 }
